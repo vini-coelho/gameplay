9a004647db8c5c542ae13010a7be16728716c038
Vini Coelho <coelho.v@pg.com>
2021-06-22 23:00:21 -0400

    chore: add navigation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import IllustrationImg from '../../assets/illustration.png';
 import { ButtonIcon } from '../../components/ButtonIcon';
@@ -6,9 +7,18 @@ import { ButtonIcon } from '../../components/ButtonIcon';
 import * as S from './styles';
 
 export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn(){
+    navigation.navigate("Home");
+  }
+
   return(
     <S.Container>
-      <S.Image source={IllustrationImg} resizeMode='stretch'/>
+      <S.Image
+        source={IllustrationImg}
+        resizeMode='stretch'
+      />
       <S.Content>
         <S.Title>
           Conecte-se {'\n'}
@@ -19,7 +29,11 @@ export function SignIn() {
           Crie grupos para jogar seus games {'\n'}
           favoritos com seus amigos
         </S.Subtitle>
-        <ButtonIcon title='Entrar com Discord' activeOpacity={0.7}/>
+        <ButtonIcon
+          title='Entrar com Discord'
+          activeOpacity={0.7}
+          onPress={handleSignIn}
+        />
       </S.Content>
     </S.Container>
   );
